35285e218add3650207ffbdc5e7258cc89d41997
Juan Sebastian Ulloa <jseb.ulloa@gmail.com>
2021-12-02 16:44:25 -0500

    added scikit-learn as requirement and updated build_dataset.py

--- sound_detection/build_dataset.py
+++ sound_detection/build_dataset.py
@@ -22,8 +22,6 @@ import glob
 # Set main variables
 target_fs = 24000  # target fs of project
 wl = 5  # Window length for formated rois
-spectro_nperseg = 512
-spectro_noverlap = 0
 
 #%% 
 """
@@ -31,16 +29,16 @@ spectro_noverlap = 0
 Positive samples
 ----------------
 
-Positive samples where detected manually from audio libraries and few field samples
+Positive samples where detected manually from audio libraries and field samples
 
 """
 # Set variables for positive samples
-path_annot = '/Volumes/lacie_macosx/github/soundclim_annotations/bounding_boxes/Boana prasina/'  # location of bbox annotations
+path_annot = '/Volumes/lacie_macosx/github/soundclim_annotations/bounding_boxes/'  # location of bbox annotations
 path_audio = '/Volumes/Audiolib/ML_datasets/AnuraSet/'  # location of raw audio
 path_save = '/Users/jsulloa/Downloads/tmp/'  # location to save new samples
 
 #%% Load multiple annotations from directory
-flist = glob.glob(path_annot+'*.txt')
+flist = glob.glob(path_annot+'**/*.txt')
 df = pd.DataFrame()
 for fname in flist:
     df_aux = util.read_audacity_annot(fname)
@@ -49,7 +47,7 @@ for fname in flist:
 df.reset_index(inplace=True, drop=True)
 
 #%% Post-process annotations. 
-# Select only clear vocalizations
+# Select vocalizations of a single species
 df_rois = df.loc[(df.label=='BOAPRA_CLR') | (df.label=='BOAPRA_MED'), :]
 
 #%% format rois to a fixed window
@@ -61,23 +59,17 @@ for idx, roi in df_rois.iterrows():
 rois_fmt.reset_index(inplace=True, drop=True)
 print(rois_fmt)
 
-#%% Get spectrograms of fixed length
+#%% Load audio, resample, trim, normalize and write to disk
 for idx_row, roi_fmt in rois_fmt.iterrows():
-    print(idx_row, '/', len(rois_fmt))
+    print(idx_row+1, '/', len(rois_fmt))
     full_path_audio = find_file(roi_fmt.fname, path_audio)[0]
     fname_audio = os.path.join(full_path_audio)
     s, fs = sound.load(fname_audio)
     s = sound.resample(s, fs, target_fs)
     s_trim = sound.trim(s, target_fs, roi_fmt.min_t, roi_fmt.max_t, pad=True)
     s_trim = sound.normalize(s_trim, max_db=0)
-    Sxx, tn, fn, ext = sound.spectrogram(s_trim, fs=target_fs, nperseg=spectro_nperseg,
-                                         noverlap=spectro_noverlap)
-    Sxx = util.power2dB(Sxx, db_range=60, db_gain=20)
-    # Save spectrograms
-    Sxx = np.flipud(Sxx)
-    fname_save = path_save+roi_fmt.label+'_'+str(idx_row).zfill(3)
-    io.imsave(fname_save+'.jpg', Sxx)#%%  
-    sf.write(fname_save+'.wav', s_trim, target_fs)
+    fname_save = os.path.join(path_save, roi_fmt.label+'_'+str(idx_row).zfill(3)) 
+    sound.write(fname_save+'.wav', fs=target_fs, data=s_trim, bit_depth=16)
 
 # save data frame
 rois_fmt['fname_trim'] = rois_fmt.label + '_' + rois_fmt.index.astype(str).str.zfill(3)
@@ -100,7 +92,7 @@ path_save = '../fixed_wl_dataset/UND/'  # location to save new samples
 df = pd.read_csv(path_annot)
 
 #%% Post-process annotations. 
-# Select 5 by file
+# Select 5 per file
 gby = df.groupby('fname', group_keys=False)
 # if cluster is smaller than n_max, take all samples from cluster
 n_max = 5
@@ -116,25 +108,19 @@ for idx, roi in df_rois.iterrows():
 rois_fmt.reset_index(inplace=True, drop=True)
 print(rois_fmt)
 
-#%% Get spectrograms of fixed length
+#%% Load audio, resample, trim, normalize and write to disk
 for idx_row, roi_fmt in rois_fmt.iterrows():
-    print(idx_row, '/', len(rois_fmt))
+    print(idx_row+1, '/', len(rois_fmt))
     full_path_audio = find_file(roi_fmt.fname, path_audio)[0]
     fname_audio = os.path.join(full_path_audio)
     s, fs = sound.load(fname_audio)
     s = sound.resample(s, fs, target_fs)
     s_trim = sound.trim(s, target_fs, roi_fmt.min_t, roi_fmt.max_t, pad=True)
     s_trim = sound.normalize(s_trim, max_db=0)
-    Sxx, tn, fn, ext = sound.spectrogram(s_trim, fs=target_fs, nperseg=spectro_nperseg,
-                                         noverlap=spectro_noverlap)
-    Sxx = util.power2dB(Sxx, db_range=60, db_gain=20)
-    # Save spectrograms
-    Sxx = np.flipud(Sxx)
-    fname_save = roi_fmt.label+'_'+str(idx_row).zfill(3)
-    io.imsave(path_save+fname_save+'.jpg', Sxx)#%%  
-    sf.write(path_save+fname_save+'.wav', s_trim, target_fs)
+    fname_save = os.path.join(path_save, roi_fmt.label+'_'+str(idx_row).zfill(3)) 
+    sound.write(fname_save+'.wav', fs=target_fs, data=s_trim, bit_depth=16)
 
-# save data frame
+# save dataframe
 rois_fmt['fname_trim'] = rois_fmt.label + '_' + rois_fmt.index.astype(str).str.zfill(3)
 rois_fmt.to_csv(path_save + 'rois_details.csv', index=False)
 
